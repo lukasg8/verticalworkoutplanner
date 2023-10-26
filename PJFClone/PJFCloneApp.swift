9ef3d8c6020337b485103f8d46882c644a44a23d
lukasg8 <lukasg@uchicago.edu>
2023-10-26 15:44:23 -0500

    Initial Commit

---
+++ PJFClone/PJFCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PJFCloneApp.swift
+//  PJFClone
+//
+//  Created by Lukas on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct PJFCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
