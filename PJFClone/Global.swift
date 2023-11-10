ccc64d37fea0ee108242c3ccf851da50d1ee0555
lukasg8 <lukasg@uchicago.edu>
2023-11-10 11:38:10 -0600

    initial commit

---
+++ PJFClone/Global.swift
@@ -0,0 +1,14 @@
+//
+//  Global.swift
+//  PJFClone
+//
+//  Created by Lukas on 10/26/23.
+//
+
+import Foundation
+import SwiftUI
+
+class Global {
+    static let MAX_PAGE = 100
+    static let CENTER_PAGE = 100 / 2
+}
